3698777cad844c060c145644863c409722eda7a8
madandinata <madanmr48@gmail.com>
2023-07-24 06:59:50 +0700

    update entity product

--- app/domain/products/productEntity.js
+++ app/domain/products/productEntity.js
@@ -1,1 +1,1 @@
-export const ProductEntity = ( productId, linkProduct, title, price) => ({ productId, linkProduct, title, price });
+export const ProductEntity = ( id, linkProduct, title, price) => ({ id, linkProduct, title, price });
