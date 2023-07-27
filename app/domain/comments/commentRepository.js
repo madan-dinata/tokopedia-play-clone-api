eb6a6023ebbd7c430bcc68e114a9de03eda4b855
madandinata <madanmr48@gmail.com>
2023-07-28 02:37:01 +0700

    update api comment

--- app/domain/comments/commentRepository.js
+++ app/domain/comments/commentRepository.js
@@ -1,9 +1,10 @@
 import VideoModel from "../../infrastructure/database/models/videoModel.js";
+import mongoose from "mongoose";
 
 export const getCommentsByVideoId = async (videoId) => {
     return VideoModel.find({ _id: videoId}, { comments: 1 })
 }
 
 export const postCommentByVideoId = async (videoId, username, comment) => {
-    return VideoModel.updateOne({ _id: videoId }, { $push: { comments : { username, comment } } })
+    return VideoModel.updateOne({ _id: videoId }, { $push: { comments : { _id: new mongoose.Types.ObjectId(), username, comment } } })
 }
